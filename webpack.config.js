e8d1fb788cf518e9d350490cb7861d95372ca031
kanary <kanary159357@gmail.com>
2021-11-20 16:25:07 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const isDevMode = process.env.NODE_ENV.includes('dev');
+const plugins = [
+	new webpack.EnvironmentPlugin({
+		NODE_ENV: 'development',
+	}),
+];
+if (!isDevMode) {
+	plugins.push(
+		new MiniCssExtractPlugin({
+			linkType: false,
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+		})
+	);
+}
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'public'),
+	},
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	plugins,
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				include: path.resolve(__dirname, 'src'),
+				use: [
+					isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+				],
+			},
+		],
+	},
+};
